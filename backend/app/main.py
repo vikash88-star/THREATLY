cd7b96ee82dd47c8502f30ac6690d2649b060ec3
Vikash Verma <2005vikash412@gmail.com>
2026-09-20 14:03:29 +0530

    Allow Vercel frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://127.0.0.1:5175",
+    "https://threatly-drab.vercel.app",
 ],
     allow_credentials=True,
     allow_methods=["*"],
