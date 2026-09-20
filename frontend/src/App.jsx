bf311b932fd088b90903141f393a3c9800ecf7a3
Vikash Verma <2005vikash412@gmail.com>
2026-09-20 10:06:35 +0530

    feat: finalize Threatly deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,8 @@ import InvestigationTimeline from "./components/InvestigationTimeline";
 
 import "./index.css";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE =
+  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 const DEMO_SCENARIOS = {
   phishingUrl: {
